a49612cf4d1618f6e75296c70c9584c661005622
Joy Okomowho <jokomowho@timbaobjects.com>
2016-05-13 23:48:15 +0100

    Updates to inner pages diagonal designs and inprint integration. Update to fonts and other aesthetic features. Integrated chat badge.

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+ $(function() {
+        $('.h1-banner').addClass('rubberBand');
+        $(".icons").hover(function () {
+        $(this).toggleClass("rotateIn");
+     });
+       Calendly.initBadgeWidget({url: 'https://calendly.com/sorterbox', text: 'Let&#x27;s schedule a chat', color: '#E84B7D', branding: true});
+    });
