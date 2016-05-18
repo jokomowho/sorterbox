f87cac41c1803cdb97b69a6321d45f7b74041f6a
Joy Okomowho <jokomowho@timbaobjects.com>
2016-05-18 08:27:05 +0100

    Updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,4 +4,5 @@
         $(this).toggleClass("rotateIn");
      });
        Calendly.initBadgeWidget({url: 'https://calendly.com/sorterbox', text: 'Let&#x27;s schedule a chat', color: '#E84B7D', branding: true});
+       
     });
